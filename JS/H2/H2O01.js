49cd60d2b2912faa3cfb9ae94b13e1c09a9f6a0a
Timvankooten <151274@csgaugustinus.nl>
2020-08-24 11:00:41 +0000

    asdf

--- JS/H2/H2O01.js
+++ JS/H2/H2O01.js
@@ -1,25 +1,11 @@
-var kater,kater,landschap,kever;
-var keverX;
-var keverY;
-
 function preload() {
-  kater = loadImage("images/brieck.jpg");
-  bomen = loadImage("images/bomen.jpg");
-  kever = loadImage("images/sprites/kever.png");
+   kever = loadImage("images/sprites/kever.png");
 }
 
 function setup() {
   canvas = createCanvas(450,450);
-  canvas.parent('processing');
-  keverX = 150;
-  keverY = 100;
+  
 }
 
 function draw() {
-  background('grey');  
-  //background(kater);
-  image(kater,25,25,400,400);
-  keverX+=random(-5,5);
-  keverY+=random(-5,5);
-  image(kever,keverX,keverY,30,30);
-}
+  ;
--- JS/test.js
+++ JS/test.js
@@ -1,14 +1,26 @@
+var kerverx= 0
+var kervery= 0
+
+function preload(){
+    pl= loadImage("../images/sprites/kever.png");
+}
+
 function setup() {
-  canvas = createCanvas(450,450);
-  background('silver');
-  canvas.parent('processing');
-  //noLoop();
+ canvas = createCanvas(450,450);
+ background('silver');
+ textSize(30);
+ canvas.parent('processing');
 }
 
 function draw() {
-  noStroke();
-  fill('steelblue');
-  ellipse(0,0,800);
-  fill('deepskyblue');
-  ellipse(450,450,400);
+    
+    for (var t =0; t < 9; t++){
+translate(0,50);
+    for (var nr =0; nr < 9; nr++){
+  image(pl,kerverx,0,50,50);
+  translate(50,0);
+    }
+    translate(0,50);
+}
+
 }
