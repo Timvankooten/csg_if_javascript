603c44d0e9fc991c07a65f87790ca607a54617e3
Timvankooten <57746918+Timvankooten@users.noreply.github.com>
2020-11-17 17:16:05 +0100

    Update Avatar.js

--- mijnGame/JS/class/Avatar.js
+++ mijnGame/JS/class/Avatar.js
@@ -1,19 +1,25 @@
-class Avatar{
+class Avatar {
     constructor() {
-    this.X = 100;
-    this.Y = 100;
-    this.hoogte = 50;
-    this.speed = 10;
+
+        this.schermhoogte = 900;
+        this.schermbreete = 1600;
+        this.X = 100;
+        this.Y = 100;
+        this.hoogte = 100;
+        this.breedte = 50;
+        this.speed = 9;
     }
     teken() {
         fill("white");
-        ellipse(this.X, this.Y, this.hoogte);
-        constrain(this.X, 0 + this.hoogte, windowWidth - this.hoogte);
-        constrain(this.Y, 0 + this.hoogte, windowHeight - this.hoogte);
+        rect(this.X, this.Y, this.breedte, this.hoogte);
+        this.X = constrain(this.X, 0, this.schermbreete - this.breedte);
+        this.Y = constrain(this.Y, 0 , this.schermhoogte - this.hoogte);
+        
     }
     beweeg() {
         if (keyIsDown(RIGHT_ARROW)) {
             this.X += this.speed;
+           
         }
         if (keyIsDown(LEFT_ARROW)) {
             this.X += -this.speed;
@@ -24,5 +30,7 @@ class Avatar{
         if (keyIsDown(DOWN_ARROW)) {
             this.Y += this.speed;
         }
+        
+       
     }
 }
