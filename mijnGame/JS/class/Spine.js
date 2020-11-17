d44dde410c4a8a18952cc3e66c8137d1b441bf78
Timvankooten <57746918+Timvankooten@users.noreply.github.com>
2020-11-17 17:17:16 +0100

    Create Spine.js

---
+++ mijnGame/JS/class/Spine.js
@@ -0,0 +1,30 @@
+
+class Spine {
+    constructor() { 
+      this.indiane = [];
+      this.aantal = 10;
+    }
+
+level() {
+ 
+       A1 = new Avatar();
+    A1.teken();
+    A1.beweeg();
+    spel = new Spine();
+    spel.level();
+ 
+
+for ( var v = 0; v <=aantal; v++) {
+ indiane.push(new Indiaan())}
+    A1.teken();
+    A1.beweeg();
+
+    for (v = 0; v < this.aantal; v++) {
+        this.indiane[v].teken();
+        this.indiane[v].beweeg();
+        this.indiane[v].gepakt();
+    }
+}
+}
+
+
